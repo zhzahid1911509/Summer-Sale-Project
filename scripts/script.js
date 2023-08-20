09f8a8bf4518076437907c2f03fe43321c3698ad
Zahid Hasan <67813593+zhzahid1911509@users.noreply.github.com>
2023-08-20 08:53:02 +0600

    calculations and styles updated

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,15 @@ function getValueOfElement(id){
     return elementValue;
 }
 
+function getProductData(divId, pnId, ppId){
+    document.getElementById(divId).addEventListener('click', function(){
+    const product_name = getValueOfElement(pnId);
+    addProductToCart(product_name);
+    const product_price = getValueOfElement(ppId);
+    calculateTotalPrice(product_price);
+  });
+}
+
 //add product to the cart
 function addProductToCart(productName){
     const cart = document.getElementById('product-list');
@@ -33,3 +42,37 @@ function calculateTotalPrice(productPrice){
     }
 }
 
+getProductData('product-kw1','pn-kw1','kw1-price');
+getProductData('product-kw2','pn-kw2','kw2-price');
+getProductData('product-kw3','pn-kw3','kw3-price');
+getProductData('product-sw1','pn-sw1','sw1-price');
+getProductData('product-sw2','pn-sw2','sw2-price');
+getProductData('product-sw3','pn-sw3','sw3-price');
+getProductData('product-frn1','pn-frn1','frn1-price');
+getProductData('product-frn2','pn-frn2','frn2-price');
+getProductData('product-frn3','pn-frn3','frn3-price');
+
+//reset all data
+document.getElementById('go-home').addEventListener('click', function(){
+    location.reload();
+});
+
+document.getElementById('autoFill').addEventListener('click', function(){
+    if(document.getElementById('btn-coupon').disabled == false){
+        document.getElementById('coupon-code').value = 'SELL200';
+    }
+});
+
+document.getElementById('btn-coupon').addEventListener('click',function(){
+    const couponCode = document.getElementById('coupon-code').value;
+    if(couponCode !== "SELL200") {
+      alert('Invalid Coupon');
+      return;
+    }
+    const totalPrice = getValueOfElement('total-price');
+    const totalPriceInNumber = parseFloat(totalPrice);
+    let discountValue = totalPriceInNumber*0.2;
+    document.getElementById('discount').innerText = discountValue.toFixed(2);
+    let finalTotal = totalPriceInNumber - discountValue;
+    document.getElementById('total').innerText = finalTotal.toFixed(2);
+  });
